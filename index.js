41a7696e91627b9c24db60b3709f1f6128ce567c
Mahwish Riaz-Jamil <mahwishcee@gmail.com>
2018-06-01 21:27:08 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ function prependKitten(name){
 }
 
 function removeLastKitten(){
-  var newKittens = kittens.pop();
+  var newKittens = kittens.slice(0, kittens.length - 1);
   return newKittens;
 }
 
