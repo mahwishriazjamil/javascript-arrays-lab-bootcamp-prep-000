8abf5fe66ea4af1d34a261bf5d016dca0c01a620
Mahwish Riaz-Jamil <mahwishcee@gmail.com>
2018-06-01 21:21:44 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
 const app = "I don't do much."
+
+var kittens = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendKitten(name){
+  kittens.push(name);
+  return kittens;
+}
+
+function destructivelyPrependKitten(name){
+  kittens.unshift(name);
+  return kittens;
+}
+
+function destructivelyRemoveLastKitten(){
+  kittens.pop();
+  return kittens;
+}
+
+function destructivelyRemoveFirstKitten(){
+  kittens.shift();
+  return kittens;
+}
+
+// --------
+function appendKitten(name){
+  var newKittens = [...kittens, name];
+  return newKittens;
+}
+
+function prependKitten(name){
+  var newKittens = [name, ...kittens];
+  return newKittens;
+}
+
+function removeLastKitten(){
+  var newKittens = kittens.splice(kittens.length-1, kittens.length);
+  return newKittens;
+}
+
+function removeFirstKitten(){
+ return kittens.slice(1);
+}
